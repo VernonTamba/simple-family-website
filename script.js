387a897c8c58a6277929a3f7e6e67fe218623f67
Vernon Tamba <tambzfantastico@gmail.com>
2022-08-23 15:28:43 +0700

    Added id, changed some styles, and added few functionalities

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const header = document.querySelector(".header");
 const navbarLink = document.querySelectorAll(".navbar__link a");
+const homeLink = document.querySelector("#home__link");
+const addressLink = document.querySelector("#address__link");
+const membersLink = document.querySelector("#members__link");
+const songsLink = document.querySelector("#songs__link");
 const footer = document.querySelector(".footer");
 const footerHeading = document.querySelector(".footer__closing h4");
 const footerParagraph = document.querySelector(".footer__closing p");
@@ -11,9 +15,8 @@ window.addEventListener("scroll", () => {
     navbarLink.forEach((link) => {
       link.style.color = "var(--clr-background)";
     });
-    console.log(document.documentElement.scrollHeight);
     // TODO: Find the dynamic element of scroll height
-    if (window.scrollY >= 3493) {
+    if (window.scrollY >= 3350) {
       footer.classList.add("footer__active");
       footerHeading.style.fontSize = "2.5rem";
       footerParagraph.style.fontSize = "1.5rem";
@@ -27,6 +30,24 @@ window.addEventListener("scroll", () => {
     navbarLink.forEach((link) => {
       link.style.color = "var(--clr-headline)";
     });
+  }
+
+  if (window.scrollY <= 430) {
+    homeLink.classList.add("navbar__link--active");
+    addressLink.classList.remove("navbar__link--active");
+  } else if (window.scrollY <= 1290) {
+    addressLink.classList.add("navbar__link--active");
+    homeLink.classList.remove("navbar__link--active");
+    membersLink.classList.remove("navbar__link--active");
+  } else if (window.scrollY <= 2330) {
+    membersLink.classList.add("navbar__link--active");
+    addressLink.classList.remove("navbar__link--active");
+    songsLink.classList.remove("navbar__link--active");
+  } else if (window.scrollY <= 3400) {
+    songsLink.classList.add("navbar__link--active");
+    membersLink.classList.remove("navbar__link--active");
+  } else {
+    songsLink.classList.remove("navbar__link--active");
   }
 });
 
